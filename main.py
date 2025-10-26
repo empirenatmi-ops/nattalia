1bfd1682852db0ebc3d76ece16c94c4052232faf
empirenatmi-ops <empirenatmi@gmail.com>
2025-10-26 17:16:39 +0000

    Archivos iniciales para el proyecto MakeCode

---
+++ main.py
@@ -0,0 +1,11 @@
+basic.show_number(22)
+basic.show_icon(IconNames.YES)
+
+def on_forever():
+    pass
+basic.forever(on_forever)
+basic.clear_screen()
+basic.show_string("Hello!")
+basic.show_string("que tal!")
+basic.show_number(88)
+basic.pause(100)
---
+++ main.ts
@@ -0,0 +1,10 @@
+basic.showNumber(22)
+basic.showIcon(IconNames.Yes)
+basic.forever(function on_forever() {
+    
+})
+basic.clearScreen()
+basic.showString("Hello!")
+basic.showString("que tal!")
+basic.showNumber(88)
+basic.pause(100)
